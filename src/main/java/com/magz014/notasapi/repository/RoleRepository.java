c06ba66e631d9d463d8c669f66b5b956520aac11
Manuel Alejandro <elgtorner10fega@gmail.com>
2025-06-10 22:34:07 -0600

    feat(repository): add base repositories for User, Note and Role

---
+++ src/main/java/com/magz014/notasapi/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.magz014.notasapi.repository;
+
+import com.magz014.notasapi.model.RoleModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<RoleModel, Long> {
+}
