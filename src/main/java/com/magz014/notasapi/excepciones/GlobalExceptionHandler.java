794dbde93b13e3837ffdd4d7ef4bfdf705f6f77b
Manuel Alejandro <elgtorner10fega@gmail.com>
2025-06-13 22:33:02 -0600

    feat(exception): add global exception handler

---
+++ src/main/java/com/magz014/notasapi/excepciones/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.magz014.notasapi.excepciones;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationErrors(MethodArgumentNotValidException ex){
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.BAD_REQUEST.value());
+        body.put("error", errors);
+
+        return new ResponseEntity<>(body,HttpStatus.BAD_REQUEST);
+    }
+}
